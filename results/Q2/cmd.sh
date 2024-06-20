b0a0678f46d8919b67b645990030cfb33c4d2f12
HsuChen88 <shine95763@gmail.com>
2024-06-20 16:53:22 +0800

    modify all cmd.sh and test

--- results/Q2/cmd.sh
+++ results/Q2/cmd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 gem5_path=/home/hsuchen/gem5-525ce650e1a5bbe71c39d4b15598d6c003cc9f9e
-result_path=/home/hsuchen/final_project/results/Q2
+result_path=/home/hsuchen/gem5_project/results/Q2
 
-benchmark_quicksort=/home/hsuchen/final_project/benchmark/quicksort
-benchmark_multiply=/home/hsuchen/final_project/benchmark/multiply
+benchmark_quicksort=/home/hsuchen/gem5_project/benchmark/quicksort
+benchmark_multiply=/home/hsuchen/gem5_project/benchmark/multiply
 
 arg="\
     --cpu-type=TimingSimpleCPU\
