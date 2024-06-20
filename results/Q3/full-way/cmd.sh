f75fd54366603cf13868210c286118add2783ee7
HsuChen88 <shine95763@gmail.com>
2024-06-21 02:40:02 +0800

    Replace home with  in all cmd.sh

--- results/Q3/full-way/cmd.sh
+++ results/Q3/full-way/cmd.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-gem5_path=/home/hsuchen/gem5-525ce650e1a5bbe71c39d4b15598d6c003cc9f9e
-result_path=/home/hsuchen/gem5_project/results/Q3/full-way
+gem5_path=${Home}/gem5-525ce650e1a5bbe71c39d4b15598d6c003cc9f9e
+result_path=${Home}/gem5_project/results/Q3/full-way
 
-benchmark_quicksort=/home/hsuchen/gem5_project/benchmark/quicksort
-benchmark_multiply=/home/hsuchen/gem5_project/benchmark/multiply
+benchmark_quicksort=${Home}/gem5_project/benchmark/quicksort
+benchmark_multiply=${Home}/gem5_project/benchmark/multiply
 
 arg="\
     --cpu-type=TimingSimpleCPU\
     --caches --l2cache --l3cache\
     --l1i_size=32kB --l1d_size=32kB --l2_size=128kB --l3_size=256kB\
     --mem-type=NVMainMemory\
-    --nvmain-config=/home/hsuchen/NVmain/Config/PCM_ISSCC_2012_4GB.config\
+    --nvmain-config=${Home}/NVmain/Config/PCM_ISSCC_2012_4GB.config\
     --output=${result_path}/std_output.txt\
     --l3_assoc=1\
 "
